aa0339d33b78bfd8bc49d8be7d1bd1a91cf0298e
Alimurtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-01-03 14:48:41 +0700

    thirdty commit

---
+++ 30-tuples-set/Main.py
@@ -0,0 +1,11 @@
+# Tuples dalam python tidak dapat diubah ataupun ditambahkan data
+
+dataList = [10,2,4,5,3]
+print(dataList)
+dataTuples = (7,8,9,6,7)
+print(dataTuples)
+print(dataTuples[1])
+
+# Sets dalam python sebuah koneksi yang tidak punya index
+dataSet = {10,4,5,7,3,8}
+print(dataSet)
