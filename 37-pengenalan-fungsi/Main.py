199fca2565676dedcce5a560ca23e691050821ae
Alimurtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-01-07 22:07:23 +0700

    thirtyseven commit

---
+++ 37-pengenalan-fungsi/Main.py
@@ -0,0 +1,13 @@
+# Pengenalan fungsi dalam python
+
+def hello_world():
+    print("Hello world")
+    print("Kepada Ali murtadlo")
+
+hello_world()
+hello_world()
+
+def fungsi():
+    print("Ini adalah fungsi")
+
+fungsi()
