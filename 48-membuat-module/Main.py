f570dcc2e86043456a28f7196f280da36c21e2d1
Alimurtadlo16 <ahmadalimurtadloasadillah@gmail.com>
2026-01-09 19:13:46 +0700

    fourtyeight commit

---
+++ 48-membuat-module/Main.py
@@ -0,0 +1,10 @@
+# Module matematika dengan import
+
+import Matematika 
+
+hasilTambah = Matematika.tambah(6,7,8,9,99)
+print(f"Hasil tambah: {hasilTambah}")
+hasilKali = Matematika.kali(6,7,8,9,99)
+print(f"Hasil kali: {hasilKali}")
+pangkat3 = Matematika.pangkat(3)
+print(f"Hasil pangkat: {pangkat3(3)}")
---
+++ 48-membuat-module/MainFrom.py
@@ -0,0 +1,11 @@
+# Module matematika dengan import from
+
+from Matematika import tambah, kali, pangkat
+from Matematika import *
+
+hasilTambah = tambah(6,7,8,9,99)
+print(f"Hasil tambah: {hasilTambah}")
+hasilKali = kali(6,7,8,9,99)
+print(f"Hasil kali: {hasilKali}")
+pangkat3 = pangkat(3)
+print(f"Hasil pangkat: {pangkat3(3)}")
